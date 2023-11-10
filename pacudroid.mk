302c2012e21f71408c9f28c91609777c7480ce2d
Thales Silva <thales.tas@gmail.com>
2023-11-10 15:14:09 +0000

    change the pacudroid inherits
    Signed-off-by: Thales Silva <thales.tas@gmail.com>

--- pacudroid.mk
+++ pacudroid.mk
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, device/pacu/pacudroid/device.mk)
 
 PRODUCT_NAME := pacudroid
